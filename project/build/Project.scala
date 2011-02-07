898890a4b718062097b3f5e67feb76f0083a8c34
softprops <d.tangren@gmail.com>
2011-02-06 22:45:22 -0500

    give it a home

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,4 +1,6 @@
 import sbt._
 class Project(info: ProjectInfo) extends PluginProject(info) {
   val dbjs = "net.databinder" %% "dispatch-lift-json" % "0.7.8"
+  override def managedStyle = ManagedStyle.Maven
+  lazy val publishTo = Resolver.file("lessis repo", new java.io.File("/var/www/repo"))
 }
